18ff9e2d6c3ad5432d9740380403dc0be87a2ceb
gangerman <118818981+gangerman@users.noreply.github.com>
2023-10-09 14:55:40 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("radie");
+        Scanner tgb = new Scanner(System.in);
+
+        Double a;
+        Double v;
+        Double r = tgb.nextDouble();
+
+        a =4*3.14*r*r;
+        System.out.print(a);
+
+    }
+}
